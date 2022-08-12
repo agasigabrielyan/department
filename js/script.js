3388e4513ae91d3f657a7b65f9cf892cba8e003f
agasi <agasi79@list.ru>
2022-08-12 15:29:25 +0300

    Добавить jquery, добавить scroll на главной

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
 let Department = (function() {
+
+    const CommonPage = {
+        link: ('.header_main_menu__link')
+    }
+
+    let init = function() {
+        $(document).on('click',CommonPage.link, scrollToAnchor);
+    }
+
+
+
+    function scrollToAnchor(event) {
+        event.preventDefault();
+        let dataId = this.dataset.id;
+        let element = document.getElementById(dataId);
+
+        // получим top и left позиции в браузере текущего загруженного урока
+        let top = element.offsetTop;
+        let left = element.offsetLeft;
+
+        // скроллим до текущего загруженного элемента
+        setTimeout(function() {
+            window.scrollTo(
+                {top: top,left: left,behavior: 'smooth'}
+            );
+        }, 250);
+
+    }
+
  
- let init = function() {
 
- }
 
- return {
-  init: init
- }
+
+
+
+
+    return {
+        init: init
+    }
  
 })();
 
