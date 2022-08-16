99cf15cbbff4b71b9b8304336e9cd0eebb41bada
agasi <agasi79@list.ru>
2022-08-16 12:21:33 +0300

    Создать анимацию цифр, при скролле до нужного элемента

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ let Department = (function() {
         link: ('.header_main_menu__link'),
         navicon: ('#nav-icon3'),
         mainmenu: ('.menu-wrapper'),
-        /* main page boxes */
+        /* main page boxes and data */
+        digitsActed: false,
         aboutDocuments: ("#about-documents"),
         aboutDepartment: ("#about-department"),
         aboutStatistics: ("#about-statistics"),
         aboutStructure: ("#about-structure"),
+        statisticDigit: (".statistic-digit")
         /* end: main page boxes */
     }
 
@@ -18,6 +20,9 @@ let Department = (function() {
         $(document).on('click', CommonPage.navicon, toggleNavIcon);
         $(document).on('scroll', CommonPage.mainbody, changeDigits);
     }
+
+
+    /** begin: methods **/
 
     function scrollToAnchor(event) {
         event.preventDefault();
@@ -42,25 +47,46 @@ let Department = (function() {
     }
 
     function changeDigits() {
-        let offsetTopPositionOfStatiscs = $(CommonPage.aboutStatistics)[0].offsetTop;
-        let windowScrollTopPosition = window.scrollTop;
-        let alreadyScrolled = window.scrollY;
-
-
-        console.log(offsetTopPositionOfStatiscs);
-        console.log(alreadyScrolled);
-        console.log('-------------------------------')
-        let difference = parseInt(offsetTopPositionOfStatiscs - alreadyScrolled);
-        console.log('Осталось доскроллить ' + (offsetTopPositionOfStatiscs - alreadyScrolled) + 'px');
-        if(difference <= 0) {
-            alert('Салют');
+        if(isScrolledIntoView(CommonPage.aboutStatistics)) {
+            changeDigitValues();
         }
-
     }
 
+    function changeDigitValues() {
+        let statisticDigits = $(CommonPage.statisticDigit);
+
+        statisticDigits.each(function() {
+            if(!CommonPage.digitsActed) {
+                let valueToReach = this.dataset.value;
+                let currNum = 0;
+                let speed = parseInt((1 / parseInt(valueToReach)) * 1000);
+
+                setInterval(() => {
+                  if(currNum < valueToReach) {
+                    currNum += 1;
+                    this.innerText = currNum;
+                  }
+                }, speed); // time in miliseconds.
 
 
 
+            }
+        });
+
+        CommonPage.digitsActed = true;
+    }
+
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    /** end: methods **/
 
 
     return {
